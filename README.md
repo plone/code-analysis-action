baa6621ae9ccde681dff9a00e1c9f0d0ef3d2ffe
Érico Andrei <ericof@gmail.com>
2022-04-12 19:03:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# kitconcept Code Analysis
+
+This action provides many checks for Plone backend code.
+
+## Inputs
+
+## `check`
+
+**Required** Type of check to run.
+
+Possible checks:
+
+* black
+* isort
+* flake8
+* pyroma
+* zpretty
+
+## `path`
+
+**Required** Path to check. Default `"src"`.
+
+## Example usage
+
+uses: kitconcept/code-analysis-action@v1
+with:
+  check: 'black'
+  path: 'src setup.py'
---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+CHECK=$1
+CHECK_PATH=$2
+
+if [[ "${CHECK}" == "black" ]]; then
+  black "${CHECK_PATH}" --check --diff
+elif  [[ "${CHECK}" == "isort" ]]; then
+  isort --check-only "${CHECK_PATH}"
+elif  [[ "${CHECK}" == "flake8" ]]; then
+  flakeheaven lint "${CHECK_PATH}"
+elif  [[ "${CHECK}" == "pyroma" ]]; then
+  pyroma -n 10 -d "${CHECK_PATH}"
+elif  [[ "${CHECK}" == "zpretty" ]]; then
+  find "${CHECK_PATH}" -name "*.zcml" -print0| xargs -0 -r zpretty -i -z --check
+  find "${CHECK_PATH}" -name "*.xml" -print0| xargs -0 -r zpretty -i -x --check
+else
+  # Custom
+  exec "$@"
+fi
